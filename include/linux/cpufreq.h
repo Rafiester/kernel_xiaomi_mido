033e2fdcc6f0e4cd582a1b67030e88503ab2a816
Damar131 <rafi.haqqi1@gmail.com>
2019-10-04 15:18:50 +0700

    cpufreq: remove cafactive gov
    Signed-off-by: Damar131 <rafi.haqqi1@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -502,13 +502,6 @@ void cpufreq_unregister_governor(struct cpufreq_governor *governor);
 #ifdef CONFIG_CPU_FREQ_GOV_PERFORMANCE
 extern struct cpufreq_governor cpufreq_gov_performance;
 #endif
-#ifdef CONFIG_CPU_FREQ_GOV_INTERACTIVE
-extern unsigned int cpufreq_interactive_get_hispeed_freq(int cpu);
-#endif
-#ifdef CONFIG_CPU_FREQ_GOV_CAFACTIVE
-extern unsigned int cpufreq_cafactive_get_hispeed_freq(int cpu);
-extern void cafactive_boost_ondemand(int cpu, s64 miliseconds, bool static_switch);
-#endif
 #ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_performance)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE)
@@ -529,9 +522,6 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE)
-extern struct cpufreq_governor cpufreq_gov_cafactive;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
